585ef81b76055cbd066a88e0b5599ec88f4f0d23
Robert <chaolong_chen@hotmail.com>
2018-09-28 23:38:57 +0800

    To design ATM Layout and do a basic Account authentication.

--- LoginActivity.java
+++ LoginActivity.java
@@ -2,12 +2,39 @@ package com.rff.atm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText edAcct;
+    private EditText edPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        findViews();
+    }
+
+    private void findViews() {
+        edAcct = findViewById(R.id.et_acct);
+        edPass = findViewById(R.id.et_pass);
+    }
+
+    public void login(View view)
+    {
+        String acct = edAcct.getText().toString();
+        String pass = edPass.getText().toString();
+
+        if ("Robert".equals(acct) && "1234".equals(pass)) {
+            setResult(RESULT_OK);
+            finish();
+        }
+    }
+
+    public void cancel(View view)
+    {
+
     }
 }
--- MainActivity.java
+++ MainActivity.java
@@ -1,6 +1,8 @@
 package com.rff.atm;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,10 +13,28 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_LOGIN = 100;
+    boolean logon = false;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_LOGIN) {
+            if (resultCode != RESULT_OK)
+                finish();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (!logon) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivityForResult(intent, REQUEST_LOGIN);
+//            startActivity(intent);
+        }
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
