d716db98f31be437b8a0e43d5963ad149d63057d
Robert <chaolong_chen@hotmail.com>
2018-09-29 22:46:11 +0800

    1. To do authentication with Firebase realtime database. 2. To use getSharedPreference to save data in android.

--- LoginActivity.java
+++ LoginActivity.java
@@ -1,12 +1,21 @@
 package com.rff.atm;
 
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = LoginActivity.class.getSimpleName();
     private EditText edAcct;
     private EditText edPass;
 
@@ -14,7 +23,21 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // File Write to Android
+        /*getSharedPreferences("atm", MODE_PRIVATE)
+                .edit()
+                .putInt("Level", 3)
+                .putString("NAME", "Robert")
+                .commit();*/
+        // File Read from Android
+        /*int level = getSharedPreferences("atm", MODE_PRIVATE)
+                .getInt("Level", 0);*/
+        /*Log.d(TAG, "onCreate: " + level);*/
         findViews();
+        String acct = getSharedPreferences("atm", MODE_PRIVATE)
+                .getString("USERID", "");
+        if (!acct.isEmpty())
+            edAcct.setText(acct);
     }
 
     private void findViews() {
@@ -24,13 +47,34 @@ public class LoginActivity extends AppCompatActivity {
 
     public void login(View view)
     {
-        String acct = edAcct.getText().toString();
-        String pass = edPass.getText().toString();
+        final String acct = edAcct.getText().toString();
+        final String pass = edPass.getText().toString();
+        FirebaseDatabase.getInstance().getReference("users").child(acct).child("password")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String pw = (String)dataSnapshot.getValue();
+                        if (pw.equals(pass)){
+                            getSharedPreferences("atm", MODE_PRIVATE)
+                                    .edit()
+                                    .putString("USERID", acct)
+                                    .apply();
+                            setResult(RESULT_OK);
+                            finish();
+                        } else {
+                            new AlertDialog.Builder(LoginActivity.this)
+                                    .setTitle("Error")
+                                    .setMessage("Login Fail")
+                                    .setPositiveButton("OK", null)
+                                    .show();
+                        }
+                    }
 
-        if ("Robert".equals(acct) && "1234".equals(pass)) {
-            setResult(RESULT_OK);
-            finish();
-        }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
     }
 
     public void cancel(View view)
